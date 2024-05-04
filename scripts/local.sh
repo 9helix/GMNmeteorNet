2e28d0f566bd69eca5d44c4b042ff748c1da8842
Dino Gržinić <grzinicdino@gmail.com>
2024-05-05 00:44:57 +0200

    Increased dataset size

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Download the dataset from the server
-scp -i /home/helix/.ssh/rms dgrzinic@gmn.uwo.ca:mldataset.tar.bz2 .
+scp -i /home/helix/.ssh/rms dgrzinic@gmn.uwo.ca:GMNmeteorNet/mldataset.tar.bz2 .
 rm mldataset.tar
+rm -rf mldataset
 bzip2 -d mldataset.tar.bz2 && tar -xf mldataset.tar
